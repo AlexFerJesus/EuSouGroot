81c88b9d2a5aafbd09abb4bf2af051401824c526
AlexFerJesus <141464221+AlexFerJesus@users.noreply.github.com>
2023-12-02 00:23:32 +0000

    Pronto

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿const string Sair = "tchau";
+Console.WriteLine("+====GrootChatbot====+");
+Console.WriteLine(@"
+⠰⣶⣶⣶⣄⠀⠀⠀⢀⣀⠀⠀⣠⣄⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠻⣿⣿⣿⡀⠀⠀⣿⠿⠷⢾⡏⠉⣿⣄⢀⣿⣷⡆⠀⠀
+⢀⣠⣬⡁⢸⣿⣶⣿⣿⡇⠀⣾⡇⠀⣿⡏⠛⢻⣿⢧⣤⠀
+⢸⣿⡿⡿⢻⠇⠀⢿⣿⡇⣠⣿⡇⣼⡿⠀⠀⣼⡏⢠⣾⠀
+⢸⣿⡀⢀⣿⠀⣴⠈⣿⣿⣿⣿⣿⣿⠃⣾⣤⣿⠀⣼⣿⠀
+⢸⣿⣧⢸⣿⣧⣿⣇⣿⣿⣿⣿⣿⣿⣾⣿⣿⣏⣼⣿⣿⠀
+⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄
+⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁
+⠀⢹⣿⣿⣿⣿⣿⠉⠙⣿⣿⣿⣿⣿⡯⠉⢻⣿⣿⣿⣿⠀
+⠀⠸⣿⣿⣿⣧⡀⠀⣠⣿⣿⣿⣿⣄⠀⢀⣼⣿⣿⣿⠇⠀
+⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀
+⠀⠀⠘⣿⣿⣿⣿⣿⡿⠿⠿⢿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀
+⠀⠀⠀⠘⣿⣿⣿⣿⣷⣄⣀⣀⣠⣿⣿⣿⣿⠏⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⠿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀
+
++====================+
+");
+string pergunta;
+do
+{
+    Console.Write("Pergunta: ");
+    pergunta = Console.ReadLine()!;
+
+    string resposta = RespostaDoGroot(pergunta);
+
+    ExibeResposta(RespostaDoGroot(pergunta));
+    Console.ResetColor();
+
+} while (pergunta.Trim().ToLower() != Sair);
+
+
+void ExibeResposta(string resposta)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"                          Resposta: {resposta}");
+}
+
+string RespostaDoGroot(string pergunta)
+{
+    return pergunta.Trim().ToLower() == Sair ?
+        "Eu sou Groot!" :
+        "Eu sou Groot.";
+}
